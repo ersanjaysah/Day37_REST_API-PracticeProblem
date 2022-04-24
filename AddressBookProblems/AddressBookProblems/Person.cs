8acb91ea6d401b15daab211ca3e80038301f9a5f
sanjay <ssah6694@gmail.com>
2022-04-24 22:57:15 +0530

    [sanjay]Added: Address book File

---
+++ AddressBookProblems/AddressBookProblems/Person.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblems
+{
+    public class Person
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public int PostalCode;
+        public long PhoneNumber;
+        public string Email;
+
+        public string Result()
+        {
+            return "\nName is: " + First_Name + " " + Last_Name + "\nAddress: " + Address + "\nCity is " + City +
+                "\nState is " + State + "\nPostal code is: " + PostalCode + "\nPhone: " + PhoneNumber + "\nEmail is " + Email;
+        }
+    }
+    public class AddressBookClass
+    {
+        public List<Person> person = new List<Person>();
+
+        public void Display()
+        {
+            Console.WriteLine("\nAddressBook is: ");
+            foreach (Person per in person)
+            {
+                Console.WriteLine(per.Result());
+            }
+        }
+
+        public void addPerson(Person p)
+        {
+            person.Add(p);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"E:\BridgeLabzAssignment\Day37_PracticeProblems\AddressBookProblems\AddressBookProblems\personDetails.json", jsonData);
+        }
+
+        public AddressBookClass()
+        {
+            string json = File.ReadAllText(@"E:\BridgeLabzAssignment\Day37_PracticeProblems\AddressBookProblems\AddressBookProblems\personDetails.json");
+            if (json.Length > 0)
+            {
+                person = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+            else
+                person = new List<Person>();
+        }
+
+        public void EditPerson(string First_Name)
+        {
+            for (int i = 0; i < person.Count; i++)
+            {
+                if (person[i].First_Name == First_Name)
+                {
+                    Console.WriteLine("Enter First Name: ");
+                    person[i].First_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Last Name: ");
+                    person[i].Last_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Address ");
+                    person[i].Address = Console.ReadLine();
+                    Console.WriteLine("Enter City: ");
+                    person[i].City = Console.ReadLine();
+                    Console.WriteLine("Enter State: ");
+                    person[i].State = Console.ReadLine();
+                    Console.WriteLine("Enter pin code: ");
+                    person[i].PostalCode = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Phone Number: ");
+                    person[i].PhoneNumber = long.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Email id: ");
+                    person[i].Email = Console.ReadLine();
+                }
+            }
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"E:\BridgeLabzAssignment\Day37_PracticeProblems\AddressBookProblems\AddressBookProblems\personDetails.json", jsonData);
+        }
+
+        public void Remove(string First_Name)
+        {
+            Person pers = null;
+            foreach (Person p in person)
+            {
+                if (p.First_Name == First_Name)
+                {
+                    pers = p;
+                }
+            }
+            person.Remove(pers);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"E:\BridgeLabzAssignment\Day37_PracticeProblems\AddressBookProblems\AddressBookProblems\personDetails.json", jsonData);
+        }
+
+    }
+}
--- AddressBookProblems/AddressBookProblems/Program.cs
+++ AddressBookProblems/AddressBookProblems/Program.cs
@@ -7,6 +7,57 @@ namespace AddressBookProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the address book problem");
+            AddressBookClass ad = new AddressBookClass();
+            while (true)
+            {
+                Console.WriteLine("\nSelect Any one Operation: ");
+                Console.WriteLine("1. To Add New Person: ");
+                Console.WriteLine("2. To Display Contacts: ");
+                int read = int.Parse(Console.ReadLine());
+                switch (read)
+                {
+                    case 1:
+                        Person person = new Person();
+                        Console.WriteLine("Enter First Name: ");
+                        string fname = Console.ReadLine();
+                        person.First_Name = fname;
+
+                        Console.WriteLine("Enter Last Name: ");
+                        string lname = Console.ReadLine();
+                        person.Last_Name = lname;
+
+                        Console.WriteLine("Enter Address ");
+                        string address = Console.ReadLine();
+                        person.Address = address;
+
+                        Console.WriteLine("Enter City: ");
+                        string city = Console.ReadLine();
+                        person.City = city;
+
+                        Console.WriteLine("Enter State: ");
+                        string state = Console.ReadLine();
+                        person.State = state;
+
+                        Console.WriteLine("Enter pin code: ");
+                        int pin = int.Parse(Console.ReadLine());
+                        person.PostalCode = pin;
+
+                        Console.WriteLine("Enter Phone Number: ");
+                        long number = long.Parse(Console.ReadLine());
+                        person.PhoneNumber = number;
+
+                        Console.WriteLine("Enter Email id: ");
+                        string email = Console.ReadLine();
+                        person.Email = email;
+
+                        ad.addPerson(person);
+                        break;
+
+                    case 2:
+                        ad.Display();
+                        break;
+                }
+            }
         }
     }
 }
